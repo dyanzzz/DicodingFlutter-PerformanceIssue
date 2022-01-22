b9072dd7ff291a45fff5a2decda8679233142fdb
Dian Tri Budianto <dyanzzz@gmail.com>
2022-01-22 07:35:47 +0700

    first commit

---
+++ lib/first_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  static const routeName = 'firstPage';
+
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final items = List<int>.generate(10000, (i) => i);
+    return Scaffold(
+      /* body: ListView(
+        children: [
+          for (var i = 0; i < 1000; i++) _buildListTile(i),
+        ],
+      ), */
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return _buildListTile(items[index]);
+        },
+      ),
+    );
+  }
+
+  Widget _buildListTile(int i) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.redAccent,
+          ),
+          const SizedBox(width: 16),
+          Text('item $i'),
+        ],
+      ),
+    );
+  }
+}
